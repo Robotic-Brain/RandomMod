3faf0a8bcf38a6deabbe75de75d2b4175b8088e5
Robotic-Brain <github@roboticbrain.de>
2013-04-20 16:59:52 +0200

    Initial Commit

---
+++ common/de/roboticbrain/randommod/RandomMod.java
@@ -0,0 +1,34 @@
+package de.roboticbrain.randommod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import de.roboticbrain.randommod.lib.Reference;
+
+@Mod(modid = Reference.MOD_ID,
+        name = Reference.MOD_NAME,
+        version = Reference.VERSION_STRING)
+@NetworkMod(clientSideRequired = true,
+                serverSideRequired = false)
+public class RandomMod {
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
